0509e57346de097af0668508f45c1363eda6cd7f
Mathieu <mathieu@vormkracht10.nl>
2020-04-05 16:21:02 +0200

    Init peer

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -4,19 +4,23 @@ class Video extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.video = React.createRef();
   }
 
   componentDidMount() {
-    this.video.current.srcObject = this.props.stream;
-    this.video.current.play();
+      this.video.current.srcObject = this.props.stream;
+      this.video.current.play();
+    // this.state.peer.connection.on('stream', (remoteStream) => {
+
+    // });
   }
 
   render() {
     return (
-        <div>aaa
+        <div>
+          {this.props.type}:
             <video className="bg-red-200" ref={this.video}></video>
-            bb
         </div>
     )
   }
--- src/Pages/VideoCall.js
+++ src/Pages/VideoCall.js
@@ -4,36 +4,146 @@ import Video from '../Components/Video';
 
 class VideoCall extends React.Component {
 
+  peer;
+  peers = [];
+
   constructor(props) {
     super(props);
     this.startVideo = this.startVideo.bind(this);
+    this.connectPeer = this.connectPeer.bind(this);
+    this.callPeer = this.callPeer.bind(this);
+
+    this.peerIdInput = React.createRef();
+    this.myVideo = React.createRef();
 
     this.state = {
-      status: 'loading',
-      peer: Object,
-      peerId: '',
-      peers: [
-      ]
-    };
-
-    this.state.peer = new Peer();
-    this.state.peer.on('connection', (conn) => {
-      conn.on('data', (data) => {
-        // Will print 'hi!'
-        console.log(data);
-      });
-      conn.on('open', () => {
-        console.log('open');
-      });
-      conn.on('close', () => {
-        console.log('close');
-      });
-    });
+      id: '',
+      stream: '',
+      streams: []
+    }
 
   }
 
   componentDidMount() {
     this.startVideo();
+    this.connect();
+  }
+
+  /**
+   * Connect to the peer2peer server for an id
+   */
+  connect() {
+
+    this.peer = new Peer();
+
+    this.peer.on('open', (id) => {
+      this.setState({
+        id: id
+      });
+    });
+
+    this.peer.on('call', (call) => {
+      console.log('someone is calling, can I get the ID? ', call);
+      let caller = this.findPeerById(call.peer);
+      // this.createPeer(connection);
+      this.answerPeer(call);
+    });
+
+    this.peer.on('connection', (dataConnection) => {
+      console.log('connection', dataConnection);
+      let newPeer = this.createPeer(dataConnection);
+    });
+  }
+
+  /**
+   * Creates a peer object
+   * @param {*} dataConnection 
+   */
+  createPeer(dataConnection) {
+    let peer = {
+      id: dataConnection.peer,
+      call: {}, // The call peer thing
+      stream: '',
+      dataConnection: dataConnection
+    }
+
+    dataConnection.on('data', (data) => {
+      console.log('Received data: ', data, dataConnection);
+    });
+    dataConnection.on('open', () => {
+      // dataConnection.send({
+      //   type: 'call',
+      //   peerId: this.state.id
+      // });
+      this.callPeer(dataConnection.peer);
+    });
+
+    this.peers.push(peer);
+    return peer;
+  }
+
+  findPeerById(id) {
+    for (let i = 0; i < this.peers.length; i++) {
+      if (this.peers[i].id === id) {
+        return this.peers[i];
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Connect a peer to the host
+   */
+  answerPeer(call) {
+
+    call.answer(this.state.stream);
+
+    call.on('stream', (stream) => {
+      // `stream` is the MediaStream of the remote peer.
+      // Here you'd add it to an HTML video/canvas element.
+      this.setState(prevState => ({
+        streams: [...prevState.streams, stream]
+      }));
+    });
+
+  }
+
+  /**
+   * Call another peer with a given peer id
+   * @param {} peerId 
+   */
+  connectPeer(peerId) {
+    peerId = peerId || this.peerIdInput.current.value;
+    if (peerId === '') {
+      return;
+    }
+    
+    // Step one: Create a connection
+    let peerConnection = this.peer.connect(peerId);
+    this.createPeer(peerConnection);
+  }
+
+  /**
+   * After a connection is established, we can call
+   * @param {} peerId 
+   */
+  callPeer(peerId) {
+    // Step one: Create a connection
+    let peer = this.findPeerById(peerId);
+    if (peer === null) {
+      console.warn('peer not found.');
+      return;
+    }
+    
+    peer.call = this.peer.call(peerId, this.state.stream);
+    // peer.call.on('stream', (stream) => {
+    //   // `stream` is the MediaStream of the remote peer.
+    //   // Here you'd add it to an HTML video/canvas element.
+    //   // @todo do something with removing it as well.
+    //   this.setState(prevState => ({
+    //     streams: [...prevState.streams, stream]
+    //   }));
+    // });
   }
 
   startVideo() {
@@ -41,17 +151,18 @@ class VideoCall extends React.Component {
     if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then( (stream) => {
-          let peers = this.state.peers;
-          peers.push({
+          this.setState({
             stream: stream
           });
-          this.setState({
-            peers: peers
-          });
-          console.log(this.state.peers);
+          this.myVideo.current.srcObject = stream;
+          this.myVideo.current.play();
+          let peerId = window.location.hash.substr(1);
+          if (peerId.length > 1) {
+            this.peerIdInput.current.value = peerId;
+            this.connectPeer();
+          }
         })
         .catch( (error) => {
-          this.state.status = 'Error loading video.';
         });
     }
 
@@ -60,11 +171,13 @@ class VideoCall extends React.Component {
   render() {
     return (
         <div>
-            Status: {this.state.status} | 
-            Your Video ID: {this.state.peerId}<br />
-            Send this link to join your call: peervideo.nl/join#{this.state.peerId}.<br />
-            {this.state.peers.map((peer, index) => {
-                    return <div>test<Video stream={peer.stream} key={ index }></Video>bcd</div>;
+            Your Video ID: {this.state.id}<br />
+            Send this link to join your call: peervideo.nl/start#{this.state.id}.<br />
+            <input type="text" ref={this.peerIdInput} placeholder="Friends Peer ID" />
+            <button onClick={() => this.connectPeer()}>Call peer</button>
+            <video ref={this.myVideo}></video>
+            {this.state.streams.map((stream, index) => {
+                    return <Video stream={stream} key={ index }></Video>;
                   })}
         </div>
     )
